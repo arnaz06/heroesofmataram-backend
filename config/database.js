1a798a0578e46a572592fb76503dfe2d74d1e8a9
sikun <arnazadiputra@gmail.com>
2017-12-12 22:57:42 +0700

    add .env.example and config/database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 module.exports =
 {
-  dbUrl:process.env.MONGO_URL || 'heroesofMataramBackendDev'
+  dbUrl:process.env.MONGO_URL || "mongodb://localhost:27017/heroesofMataramBackendDev"
 }
