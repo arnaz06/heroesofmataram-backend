726801b1a2cd54b439a0a3d77bf1e2f796737b43
sikun <arnazadiputra@gmail.com>
2017-12-17 19:36:35 +0700

    remove swagger-express

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
-var swagger = require('swagger-express')
 var jwt = require('jsonwebtoken');
 var crypto = require('crypto')
 
